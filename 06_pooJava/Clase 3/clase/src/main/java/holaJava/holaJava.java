d33a73d5235ff15eaae605763966a5d9c9d60a0b
santos <santos.sanchez@bue.edu.ar>
2022-06-14 15:33:52 -0300

    Java Clase 3

---
+++ 06_pooJava/Clase 3/clase/src/main/java/holaJava/holaJava.java
@@ -0,0 +1,5 @@
+package holaJava;
+
+public class holaJava {
+
+}
---
+++ 06_pooJava/Clase 3/clase/src/main/java/model/Alumno.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.math.BigInteger;
+
+public class Alumno {
+	String nombre;
+	String apellido;
+	BigInteger dni;
+	String telefono;
+	
+	
+	private void inscribirseAMateria() {
+		// TODO Auto-generated method stub
+		Math.sqrt(16);
+	}
+	
+	
+	
+}
